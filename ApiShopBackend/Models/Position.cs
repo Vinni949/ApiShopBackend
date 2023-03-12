bcc93f0e63fe41a9cb5fae624829d98fb63d0c2c
Vinni949 <pumba0@yandex.ru>
2023-03-12 12:13:30 +0300

    AddPosition

---
+++ ApiShopBackend/Models/Position.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiShopBackend.Models
+{
+    public class Position
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public string IMG { get; set; }
+    }
+}
