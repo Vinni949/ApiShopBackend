4658177abd55be12813b6778f255384b8ba995a1
Vinni949 <pumba0@yandex.ru>
2023-03-12 12:10:17 +0300

    AddOrders

---
+++ ApiShopBackend/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiShopBackend.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Organization Organization { get; set; }
+        public string CounterpartyId { get; set; }
+        public List<string>? PurchasesId { get; set; }
+        public List<string>? PositionsId { get; set; }
+        public string? Comment { get; set; }
+    }
+}
--- ApiShopBackend/Models/Organization.cs
+++ ApiShopBackend/Models/Organization.cs
@@ -2,5 +2,12 @@
 {
     public class Organization
     {
+        public int Id { get; set; }
+        public string Name { get; set; }   
+        public string? Adress { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public List<string>? OrdersId { get; set; }
+        public List<string>? PurchasesId { get; set; }
     }
 }
