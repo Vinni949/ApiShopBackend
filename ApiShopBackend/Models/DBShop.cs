0111ef97e65f9523e3da24bd900abdf688aba08e
Vinni949 <pumba0@yandex.ru>
2023-03-12 21:55:05 +0300

    AddDBShop

---
+++ ApiShopBackend/Models/DBShop.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace ApiShopBackend.Models
+{
+    public class DBShop:DbContext
+    {
+        
+        public DBShop(DbContextOptions<DBShop> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Shop> Shop { get; set; }
+        public DbSet<Organization> Organizations { get; set; }
+        public DbSet<Counterparty> Counterparty { get; set; }
+        public DbSet<Order> Order { get; set; }
+        public DbSet<Purchase> Purchase { get; set; }
+        public DbSet<Position> Positions { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=DBShop;Trusted_Connection=True;");
+
+        }
+    }
+}
